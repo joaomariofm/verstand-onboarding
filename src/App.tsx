ffc25ce770e3942a46cf38a8c524013b08ddbeec
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-07-15 17:22:35 -0300

    adding shadcn/ui table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
+import {
+	Table,
+	TableHeader,
+	TableCaption,
+	TableRow,
+	TableHead,
+	TableBody,
+	TableCell
+} from "./components/ui/table"
+
 function App() {
   return (
-    <>
-			<h1>Hello World!</h1>
-    </>
+		<>
+			<div className="mt-5 max-w-5xl mx-auto">
+				<h1 className="text-4xl font-bold">Produtos</h1>
+
+				<div className="py-4">
+					<Table>
+
+						<TableHeader>
+							<TableRow>
+								<TableHead className="w-[100px]">Invoice</TableHead>
+								<TableHead>Status</TableHead>
+								<TableHead>Method</TableHead>
+								<TableHead className="text-right">Amount</TableHead>
+							</TableRow>
+						</TableHeader>
+
+						<TableBody>
+							{
+								Array.from({ length: 10 }).map((_, i) => (
+									<TableRow key={i}>
+										<TableCell className="font-medium">INV001</TableCell>
+										<TableCell>Paid</TableCell>
+										<TableCell>Credit Card</TableCell>
+										<TableCell className="text-right">$250.00</TableCell>
+									</TableRow>
+								))
+							}	
+						</TableBody>
+
+						<TableCaption>A list of your recent invoices.</TableCaption>
+					</Table>
+				</div>
+			</div>
+		</>
   )
 }
 
