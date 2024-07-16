162788fe9b0a6c6bb0ad2db18c7dea2940ef2de4
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-07-16 18:15:06 -0300

    reorganizing backend files and folders

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MainModule } from './main.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function main() {
+  const app = await NestFactory.create(MainModule);
   await app.listen(3000);
 }
-bootstrap();
+
+main();
