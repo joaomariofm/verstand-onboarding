34d01f2ffdab0127bd19bbfd453163bceef39b63
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-07-15 22:02:47 -0300

    creating custom product dialog

--- src/components/AddProductDialog.tsx
+++ src/components/AddProductDialog.tsx
@@ -1,6 +1,8 @@
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
 import { GoPlusCircle } from "react-icons/go"
 import { Button } from "@/components/ui/button"
-import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import {
   Dialog,
@@ -11,56 +13,112 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+} from "@/components/ui/form"
+
+const formSchema = z.object({
+	name: z.string(),
+	price: z.string().transform(value => Number(value)||0),
+	quantity: z.string().transform(value => Number(value)||0),
+})
 
 function AddProductDialog() {
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: "",
+			price: 0,
+			quantity: 0,
+		},
+	})
+
+	function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+	
 	return (
 		<>
 			<Dialog>
+
 				<DialogTrigger className="w-full" asChild>
 					<Button>
 						<GoPlusCircle className="h-[18px] w-[18px] mr-2"/>
 						Adicionar produto
 					</Button>
 				</DialogTrigger>
+
 				<DialogContent className="sm:max-w-[425px]">
+
 					<DialogHeader>
 						<DialogTitle>Adicionar Produto</DialogTitle>
 						<DialogDescription>
 							Preencha os campos abaixo para adicionar um novo produto.
 						</DialogDescription>
 					</DialogHeader>
-					<div className="grid gap-4 py-4">
-						<div className="grid grid-cols-4 items-center gap-4">
-							<Label htmlFor="name" className="text-right">
-								Nome
-							</Label>
-							<Input
-								id="name"
-								className="col-span-3"
-							/>
-						</div>
-						<div className="grid grid-cols-4 items-center gap-4">
-							<Label htmlFor="price" className="text-right">
-								Preço
-							</Label>
-							<Input
-								id="price"
-								className="col-span-3"
-							/>
-						</div>
-						<div className="grid grid-cols-4 items-center gap-4">
-							<Label htmlFor="quantity" className="text-right">
-								Quantidade
-							</Label>
-							<Input
-								id="quantity"
-								className="col-span-3"
-							/>
-						</div>
-					</div>
-					<DialogFooter>
-						<Button type="submit">Adicionar</Button>
-					</DialogFooter>
+
+					<Form {...form}>
+						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+							<div className="grid gap-4 py-4">
+
+								<FormField
+									control={form.control}
+									name="name"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Nome</FormLabel>
+											<FormControl>
+												<Input placeholder="Nome do produto" {...field} />
+											</FormControl>
+										</FormItem>
+									)}
+								/>
+
+								<FormField
+									control={form.control}
+									name="price"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Preço</FormLabel>
+											<FormControl>
+												<Input
+													{...field}
+													placeholder="Preço do produto"
+												/>
+											</FormControl>
+										</FormItem>
+									)}
+								/>
+
+								<FormField
+									control={form.control}
+									name="quantity"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Quantidade</FormLabel>
+											<FormControl>
+												<Input
+													{...field}
+													placeholder="Quantidade do produto"
+												/>
+											</FormControl>
+										</FormItem>
+									)}
+								/>
+
+							</div>
+
+							<DialogFooter>
+								<Button type="submit">Adicionar</Button>
+							</DialogFooter>
+						</form>
+					</Form>
 				</DialogContent>
 			</Dialog>
 		</>
