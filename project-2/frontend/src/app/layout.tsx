71ce324eb3392b4c320dd04aaa01dbb7a138dc8a
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-07-19 17:48:47 -0300

    creating movie cards

--- project-2/frontend/src/app/layout.tsx
+++ project-2/frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css"
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className="dark">
       <body>{children}</body>
     </html>
   )
--- project-2/frontend/src/app/movies/page.tsx
+++ project-2/frontend/src/app/movies/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import MovieService from '@/services/movie.service';
+import { Card } from '@/components/ui/card';
 import { useQuery } from '@tanstack/react-query';
+import "./index.css";
 
 export default function MoviesPage () {
 	const { data, isLoading } = useQuery({queryKey: ['movies'], queryFn: MovieService.getMovies});
@@ -12,15 +14,19 @@ export default function MoviesPage () {
 
 	if (!isLoading) {
 		return (
-			<div>
-				<h1>Movies</h1>
-				<ul>
+			<div className="p-6">
+				<ul className="flex flex-wrap justify-center gap-5">
 					{data?.map((movie) => (
 						<li key={movie.id}>
-							<img src={movie.poster} alt="movie poster" />
-							<p>{movie.title}</p>
-							<p>{movie.description}</p>
-							<p>{movie.poster}</p>
+							<Card className="max-w-64 h-96 overflow-hidden">
+								<div className="w-full h-1/3 relative z-0">
+									<img src={movie.poster} alt="movie poster" className="w-full"/>
+								</div>
+								<div className="description-container px-4 pt-3 h-2/3 relative z-10">
+									<p className="font-black text-zinc-300 mb-2">{movie.title}</p>
+									<p className="font-bold text-zinc-300">{movie.description}</p>
+								</div>
+							</Card>
 						</li>
 					))}
 				</ul>
