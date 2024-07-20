f0e5d27de3f13cb75d3e98dbbb083818c7eb2b8b
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-07-20 14:10:54 -0300

    updating movie card style

--- project-2/frontend/src/app/movies/page.tsx
+++ project-2/frontend/src/app/movies/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import MovieService from '@/services/movie.service';
-import { Card } from '@/components/ui/card';
-import { useQuery } from '@tanstack/react-query';
 import "./index.css";
+import MovieService from '@/services/movie.service';
+import { MovieCard } from '@/components/movie-card';
+import { useQuery } from '@tanstack/react-query';
 
 export default function MoviesPage () {
 	const { data, isLoading } = useQuery({queryKey: ['movies'], queryFn: MovieService.getMovies});
@@ -18,15 +18,7 @@ export default function MoviesPage () {
 				<ul className="flex flex-wrap justify-center gap-5">
 					{data?.map((movie) => (
 						<li key={movie.id}>
-							<Card className="max-w-64 h-96 overflow-hidden">
-								<div className="w-full h-1/3 relative z-0">
-									<img src={movie.poster} alt="movie poster" className="w-full"/>
-								</div>
-								<div className="description-container px-4 pt-3 h-2/3 relative z-10">
-									<p className="font-black text-zinc-300 mb-2">{movie.title}</p>
-									<p className="font-bold text-zinc-300">{movie.description}</p>
-								</div>
-							</Card>
+							<MovieCard movie={movie}/>
 						</li>
 					))}
 				</ul>
---
+++ project-2/frontend/src/components/movie-card.tsx
@@ -0,0 +1,16 @@
+import Movie from "@/models/movie";
+import { Card } from '@/components/ui/card';
+
+export function MovieCard({ movie }: {movie: Movie}) {
+	return(
+		<Card className="flex max-h-64 overflow-hidden">
+			<div>
+				<img src={movie.poster} alt="movie poster" className="w-60 lg:w-48 h-full"/>
+			</div>
+			<div className="w-72 bg-zinc-800 p-3">
+				<p className="font-black text-zinc-300 mb-2">{movie.title}</p>
+				<p className="text-sm font-bold text-zinc-300">{movie.description}</p>
+			</div>
+		</Card>
+	)
+}
