7b3bed1f455124f479b054cf786566922c7fcd21
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-07-20 14:14:02 -0300

    removing specific layout

--- project-2/frontend/src/app/layout.tsx
+++ project-2/frontend/src/app/layout.tsx
@@ -1,9 +1,12 @@
 import "./globals.css"
+import TanStackProvider from "../providers/tanstack.provider"
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-BR" className="dark">
-      <body>{children}</body>
+			<TanStackProvider>
+				<body>{children}</body>
+			</TanStackProvider>
     </html>
   )
 }
