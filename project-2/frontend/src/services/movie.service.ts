46bf6d91c7f4d30e5243744fecc87e96150d670f
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-07-20 17:35:51 -0300

    refactoring movie service

--- project-2/frontend/src/services/movie.service.ts
+++ project-2/frontend/src/services/movie.service.ts
@@ -2,7 +2,14 @@ import Movie from "@/models/movie";
 
 export default class MovieService {
 	static async getMovies(): Promise<Movie[]>{
-		const res = await fetch('http://localhost:3001/movies/list');
-		return res.json();
+		const response = await fetch('http://localhost:3001/movies/list');
+
+		const parsedResponse = await response.json();
+
+		if (parsedResponse.status !== 200) {
+			throw new Error('Something went wrong');
+		}
+
+		return parsedResponse;
 	}
 }
