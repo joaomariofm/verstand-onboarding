47f8c079a5655f5565ec5040a51419e9d6fb4e5d
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-07-22 15:57:38 -0300

    setting jwt session token

--- project-2/frontend/src/services/user.service.ts
+++ project-2/frontend/src/services/user.service.ts
@@ -1,19 +1,26 @@
+import { cookies } from 'next/headers';
 import User from "@/models/user";
+import AuthService from "./auth.service";
+
+const EXPIRING_TIME = 24 * 60 * 60 * 1000;
 
 export default class UserService {
-	static async login(email: string, password: string): Promise<User>{
+	static async login(email: string, password: string): Promise<void>{
 		const response = await fetch('http://localhost:3001/user/login', {
 			method: 'POST',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({email, password})
 		});
 
-		const parsedResponse = await response.json();
+		const { status, user } = await response.json();
 		
-		if (parsedResponse.status !== 200) {
+		if (status !== 200) {
 			throw new Error('Invalid credentials');
 		}
-	
-		return parsedResponse;
+
+		const expires = new Date(Date.now() + EXPIRING_TIME);
+		const session = await AuthService.encrypt(user as User);
+
+		cookies().set("session", session, { expires, httpOnly: true });
 	}
 }
