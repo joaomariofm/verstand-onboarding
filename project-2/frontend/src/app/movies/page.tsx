c2bd2fb8a535da3d1d3943589351eee0ee62707e
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2024-07-22 15:38:48 -0300

    removing wrong index.css import

--- project-2/frontend/src/app/movies/page.tsx
+++ project-2/frontend/src/app/movies/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import "./index.css";
 import MovieService from '@/services/movie.service';
 import MovieCard from '@/components/movie-card';
 import { useQuery } from '@tanstack/react-query';
